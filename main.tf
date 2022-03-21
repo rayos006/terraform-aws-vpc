28bbbc8a8ffd4c1a89066efaac3440af593d4ab9
raytyler <tyler.ray@liveviewtech.com>
2022-03-21 10:26:37 -0600

    fix subnet associations

--- main.tf
+++ main.tf
@@ -1144,7 +1144,7 @@ resource "aws_route_table_association" "public" {
   count = local.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
 
   subnet_id      = element(aws_subnet.public[*].id, count.index)
-  route_table_id = aws_route_table.public[0].id
+  route_table_id = element(aws_route_table.public[*].id, count.index)
 }
 
 ################################################################################
